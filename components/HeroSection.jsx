bd4d9d9171b70b98ecd9b93df8259ed61a83f08c
hopplers <siaji526@gmail.com>
2023-12-08 19:56:28 +0800

    first commit

---
+++ components/HeroSection.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import TypeWritter from "typewriter-effect";
+
+
+const HeroSection = () => {
+    return (
+        <main className="py-16 mt-16 md:flex md:shrink-0">
+            <div className="grid grid-cols-1 md:grid-cols-12">
+                <div className="col-span-1 md:col-span-8 place-self-center justify-self-start">
+                    <h1 className="fold-extrabold">
+                        <span className="text-white text-8xl">
+
+                        </span>
+                    </h1>
+                </div>
+            </div>
+        </main>
+    )
+}
